1350265f945ee11118d16c903c8dc091eda66ea3
ObaidAhmedSiddique <iteb4.obaid@gmail.com>
2022-05-24 04:09:40 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+// |||||||||||||||||||||||||||||||||||||||||||| ALERTS (Ch # 01) |||||||||||||||||||||||||||||||||||||||||||||
+
+// ***************************************** Question # 01 *****************************************
+
+// alert("Hello Java World!");
+
+// ***************************************** Question # 02 *****************************************
+
+// var password = prompt("Enter your password:");
+// alert("Error! Please enter a valid password.")
+
+// ***************************************** Question # 03 *****************************************
+
+// alert("Welcome to JS Land..." + "\n" + "Happy Coding!");
+
+// ***************************************** Question # 04 *****************************************
+
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!" + "\n" + "Prevent this page from creating additional dialogs.")
+
+
+
+// ||||||||||||||||||||||||||||||||||||||| VARIABLES FOR STRINGS (Ch # 02) |||||||||||||||||||||||||||||||||||
+
+// ***************************************** Question # 01 *****************************************
+
+// var username;
+
+// ***************************************** Question # 02 *****************************************
+
+// var myName = "Obaid Ahmed Siddique";
+
+// ***************************************** Question # 03 *****************************************
+
+// var message = "Hello World";
+// alert(message);
+
+// ***************************************** Question # 04 *****************************************
+
+// var studentName = "Jhone Doe", age = "15 years old", qualification = "Certified Mobile Application Development";
+// alert(studentName);
+// alert(age);
+// alert(qualification);
+
+// ***************************************** Question # 05 *****************************************
+
+// var food = "PIZZA";
+// alert(food + "\n" + "PIZZ" + "\n" + "PIZ" + "\n" +"PI" + "\n" + "P");
+
+// ***************************************** Question # 06 *****************************************
+
+// var email = "iteb4.obaid@gmail.com";
+// alert("My email address is " + email);
+
+// ***************************************** Question # 07 *****************************************
+
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from a Book " + book);
+
+// ***************************************** Question # 08 *****************************************
+
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// ***************************************** Question # 09 *****************************************
+
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+// document.write(design);
